01944ca4a07c661da9f125d499560acfc490f259
Andy Miller <andymiller1313@gmail.com>
2017-06-25 02:28:10 -0500

    Problem 3 complete on my air cargo problems

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -379,4 +379,44 @@ def air_cargo_p2() -> AirCargoProblem:
 
 def air_cargo_p3() -> AirCargoProblem:
     # TODO implement Problem 3 definition
-    pass
+    # #This part is pretty straightforward I"m just picking the positive and negative states
+    #I'm copying the state above so my logic program knows what to do to get to the goal.
+
+    """
+    Init(At(C1, SFO) ∧ At(C2, JFK) ∧ At(C3, ATL) ∧ At(C4, ORD)
+	∧ At(P1, SFO) ∧ At(P2, JFK) ∧ Cargo(C1) ∧ Cargo(C2)
+	∧ Cargo(C3) ∧ Cargo(C4) ∧ Plane(P1) ∧ Plane(P2)
+	∧ Airport(JFK) ∧ Airport(SFO) ∧ Airport(ATL) ∧ Airport(ORD))
+
+    Goal(At(C1, JFK) ∧ At(C3, JFK) ∧ At(C2, SFO) ∧ At(C4, SFO))
+    """
+
+    #Here's all the objects we'll use:
+    airports = ['ORD', 'SFO', 'ATL', 'JFK']
+    planes = ['P1', 'P2']
+    cargos = ['C1', 'C2', 'C3', 'C4']
+
+    #Here's where we start at:
+    pos = [expr('At(C1, SFO)'), expr('At(C2. JFK)'), expr('At(C3, ATL)'),
+           expr('At(C4, ORD'), expr('At(P1, SFO)'), expr('At(P2, JFK)')]
+
+
+    #Here's where we don't want to go, literally we call every other state.
+    neg = [expr('At(P1, ATL)'), expr('At(P1. ORD)'), expr('At(P1, JFK)'),
+           expr('At(P2, ATL)'), expr('At(P2, SFO)'), expr('At(P2, ORD)'),
+           expr('At(C1, JFK)'), expr('At(C1. ATL)'), expr('At(C1, ORD)'),
+           expr('In(C1, P1)'), expr('In(C1, P2)'), expr('At(C2, SFO)'),
+           expr('At(C2, ORD)'), expr('At(C2, ATL)'), expr('In(C2, P1)'),
+           expr('In(C2, P2)'), expr('In(C3, P1)'), expr('In(C3, P2)'),
+           expr('At(C3, JFK)'), expr('At(C3, ORD)'), expr('At(C3, SFO)')
+           expr('At(C4, JFK)'), expr('At(C4, SFO)'), expr('At(C4, ORD)'),
+           expr('In(C4, P1)'), expr('In(C4, P2)')]
+
+    #Call the FluentState as our initial state (like p1)
+    init = FluentState(pos, neg)
+
+    #This is the goal.
+    goal = [expr('At(C1, JFK)'), expr('At(C3, JFK)'), expr('At(C2, SFO)'), expr('At(C4, SFO')]
+
+    #return and call function.
+    return AirCargoProblem(cargos, planes, airports, init, goal)
